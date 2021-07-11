a1a175e97500d54c92cc925fdb84aa8881f0d73c
plotinusspascual <plotinusspascual@gmail.com>
2021-07-10 22:06:03 -0700

    21 Merge Two Sorted List

---
+++ MergeSortedList.java
@@ -0,0 +1,63 @@
+import java.util.List;
+import java.lang.Object;
+import java.util.*;
+
+public class MergeSortedList {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode tempNode = new ListNode(0);
+        ListNode currentNode = tempNode;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                currentNode.next = l1;
+                l1 = l1.next;
+            } else {
+                currentNode.next = l2;
+                l2 = l2.next;
+            }
+            currentNode = currentNode.next;
+        }
+
+        if (l1 != null) {
+            currentNode.next = l1;
+            l1 = l1.next;
+        }
+
+        if (l2 != null) {
+            currentNode.next = l2;
+            l2 = l2.next;
+        }
+        return tempNode.next;
+    }
+
+    static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        MergeSortedList test = new MergeSortedList();
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(3);
+        head1.next.next = new ListNode(5);
+
+        ListNode head2 = new ListNode(2);
+        head2.next = new ListNode(4);
+        head2.next.next = new ListNode(6);
+
+        ListNode merge = test.mergeTwoLists(head1, head2);
+        printList(merge);
+    }
+}
